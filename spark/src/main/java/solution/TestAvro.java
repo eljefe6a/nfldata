394e8b198be3c92aa421297ae7f6b3afec4ac34b
Jesse Anderson <jesse@smokinghand.com>
2015-05-06 15:48:13 -0700

    Added Avro test for Spark.

---
+++ spark/src/main/java/solution/TestAvro.java
@@ -0,0 +1,39 @@
+package solution;
+
+import model.Play;
+import model.PlayTypes;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.Arrays;
+
+public class TestAvro {
+    public static void main(String[] args) {
+        JavaSparkContext sc = new JavaSparkContext("local", "JavaAPISuite");
+
+        JavaRDD<String> lines = sc.textFile("log.txt");
+        JavaRDD<PlayTypes> plays =
+                lines.map(
+                        line -> PlayTypes.END
+                                //new Play("game", 1, 1, 1, "offense", "defense", 1, 10, 20, "play desc", 0, 0, 2000, "qb", "offensiveplayer", "defense1", "defense2",
+                                //false, false, false, false, PlayTypes.PASS, "home", "away", "dateplayed", "playid", "winner", 0, 0, false, false, false, false, false)
+                );
+        /*
+        public Play(java.lang.CharSequence Game, java.lang.Integer Quarter, java.lang.Integer GameMinutes, java.lang.Integer GameSeconds,
+                java.lang.CharSequence Offense, java.lang.CharSequence Defense, java.lang.Integer Down, java.lang.Integer YardsToGo, java.lang.Integer YardLine,
+                java.lang.CharSequence PlayDesc, java.lang.Integer OffenseScore, java.lang.Integer DefenseScore, java.lang.Integer Year, java.lang.CharSequence QB,
+                java.lang.CharSequence OffensivePlayer, java.lang.CharSequence DefensivePlayer1, java.lang.CharSequence DefensivePlayer2,
+                java.lang.Boolean Penalty, java.lang.Boolean Fumble, java.lang.Boolean Incomplete, java.lang.Boolean IsGoalGood, model.PlayTypes PlayType,
+                java.lang.CharSequence HomeTeam, java.lang.CharSequence AwayTeam, java.lang.CharSequence DatePlayed, java.lang.CharSequence PlayId,
+                java.lang.CharSequence Winner, java.lang.Integer HomeTeamScore, java.lang.Integer AwayTeamScore, java.lang.Boolean PlayerArrested,
+                java.lang.Boolean OffensePlayerArrested, java.lang.Boolean DefensePlayerArrested, java.lang.Boolean HomeTeamPlayerArrested,
+                java.lang.Boolean AwayTeamPlayerArrested) {
+                */
+            plays.collect().forEach(
+                    t -> System.out.println("Away:" + t.name())
+                    //t -> System.out.println("Away:" + t.getAwayTeam() + " Home:" + t.getHomeTeam())
+            );
+        }
+    }
