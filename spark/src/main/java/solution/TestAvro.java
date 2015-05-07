9498fb8cfd0028e4194e51b8e3ec85582de6613f
Jesse Anderson <jesse@smokinghand.com>
2015-05-07 14:33:08 -0700

    Fixed Avro test.

--- spark/src/main/java/solution/TestAvro.java
+++ spark/src/main/java/solution/TestAvro.java
@@ -2,6 +2,8 @@ package solution;
 
 import model.Play;
 import model.PlayTypes;
+import org.apache.avro.mapred.AvroWrapper;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -11,14 +13,16 @@ import java.util.Arrays;
 
 public class TestAvro {
     public static void main(String[] args) {
-        JavaSparkContext sc = new JavaSparkContext("local", "JavaAPISuite");
+        SparkConf conf = new SparkConf();
+        conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+
+        JavaSparkContext sc = new JavaSparkContext("local", "JavaAPISuite", conf);
 
         JavaRDD<String> lines = sc.textFile("log.txt");
-        JavaRDD<PlayTypes> plays =
+        JavaRDD<Play> plays =
                 lines.map(
-                        line -> PlayTypes.END
-                                //new Play("game", 1, 1, 1, "offense", "defense", 1, 10, 20, "play desc", 0, 0, 2000, "qb", "offensiveplayer", "defense1", "defense2",
-                                //false, false, false, false, PlayTypes.PASS, "home", "away", "dateplayed", "playid", "winner", 0, 0, false, false, false, false, false)
+                        line -> new Play("game", 1, 1, 1, "offense", "defense", 1, 10, 20, "play desc", 0, 0, 2000, "qb", "offensiveplayer", "defense1", "defense2",
+                                false, false, false, false, PlayTypes.PASS, "home", "away", "dateplayed", "playid", "winner", 0, 0, false, false, false, false, false)
                 );
         /*
         public Play(java.lang.CharSequence Game, java.lang.Integer Quarter, java.lang.Integer GameMinutes, java.lang.Integer GameSeconds,
@@ -31,9 +35,10 @@ public class TestAvro {
                 java.lang.Boolean OffensePlayerArrested, java.lang.Boolean DefensePlayerArrested, java.lang.Boolean HomeTeamPlayerArrested,
                 java.lang.Boolean AwayTeamPlayerArrested) {
                 */
-            plays.collect().forEach(
-                    t -> System.out.println("Away:" + t.name())
+        plays.collect().forEach(
+                    t -> System.out.println("Away:" + t.getDefense())
                     //t -> System.out.println("Away:" + t.getAwayTeam() + " Home:" + t.getHomeTeam())
             );
         }
+
     }
